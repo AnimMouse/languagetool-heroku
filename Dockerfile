9de330add405935f2d0726f1f5f2bf0a08150adc
AnimMouse <git@animmouse.com>
2022-02-06 12:08:03 +0800

    Add files

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:19-alpine
+RUN apk add --no-cache libgomp gcompat libstdc++ unzip
+ENV VERSION 5.6
+RUN wget https://languagetool.org/download/LanguageTool-$VERSION.zip && unzip LanguageTool-$VERSION.zip && rm LanguageTool-$VERSION.zip
+WORKDIR /LanguageTool-$VERSION
+COPY languagetool.sh /
+CMD ["/languagetool.sh"]
---
+++ languagetool.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir /tmp/ngrams /tmp/ngrams-tmp
+(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp && unzip /tmp/ngrams-en-20150817.zip -d /tmp/ngrams-tmp && mv /tmp/ngrams-tmp/* /tmp/ngrams && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp && echo Ngram has been set up) &
+java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port $PORT --public --allow-origin '*' --languageModel /tmp/ngrams
