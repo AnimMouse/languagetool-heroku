6bda6b51fa9a8fcbac5b971f4c1000f079497937
AnimMouse <git@animmouse.com>
2022-02-06 23:05:13 +0800

    Go back to no pipe

--- languagetool.sh
+++ languagetool.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /tmp/ngrams/ /tmp/ngrams-tmp/
-(cd /tmp/ngrams-tmp/ && wget -O - https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip | jar -x && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && echo Ngram has been set up) &
+(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp/ && unzip /tmp/ngrams-en-20150817.zip -d /tmp/ngrams-tmp/ && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp/ && echo Ngram has been set up) &
 java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port $PORT --public --allow-origin '*' --languageModel /tmp/ngrams/
