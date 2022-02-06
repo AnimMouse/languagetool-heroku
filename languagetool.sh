3d75140b3d78457b79a35f1e22f3427e67cdceb7
AnimMouse <git@animmouse.com>
2022-02-06 23:54:28 +0800

    Use jar for unzip

--- languagetool.sh
+++ languagetool.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /tmp/ngrams/ /tmp/ngrams-tmp/
-(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp/ && unzip /tmp/ngrams-en-20150817.zip -d /tmp/ngrams-tmp/ && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp/ && echo Ngram has been set up) &
+(wget https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip -P /tmp/ && cd /tmp/ngrams-tmp/ && jar -xvf /tmp/ngrams-en-20150817.zip && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && rm -r /tmp/ngrams-en-20150817.zip /tmp/ngrams-tmp/ && echo Ngram has been set up) &
 java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port $PORT --public --allow-origin '*' --languageModel /tmp/ngrams/
