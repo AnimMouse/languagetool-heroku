4dd4adba87add34c2a20a025d336ef4f837d1dce
AnimMouse <git@animmouse.com>
2022-02-06 22:51:07 +0800

    Go back to ngram on the fly

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM openjdk:19-alpine
+FROM openjdk:17-alpine
 ENV VERSION 5.6
 RUN wget -O - https://languagetool.org/download/LanguageTool-$VERSION.zip | unzip -
-RUN mkdir ngrams && cd ngrams && wget -O - https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip | jar -x
 WORKDIR /LanguageTool-$VERSION
 COPY languagetool.sh /
 CMD ["/languagetool.sh"]
--- languagetool.sh
+++ languagetool.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port $PORT --public --allow-origin '*' --languageModel /ngrams
+mkdir /tmp/ngrams/ /tmp/ngrams-tmp/
+(cd /tmp/ngrams-tmp/ && wget -O - https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip | jar -x && mv /tmp/ngrams-tmp/* /tmp/ngrams/ && echo Ngram has been set up) &
+java -Xms256m -Xmx512m -cp languagetool-server.jar org.languagetool.server.HTTPServer --port $PORT --public --allow-origin '*' --languageModel /tmp/ngrams/
